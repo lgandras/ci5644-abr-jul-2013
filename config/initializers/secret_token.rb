402d0b1912599775934b1e9a5bc63d498b2c61e5
Andras Gyomrey <lgandras@gmail.com>
2013-05-25 01:55:13 -0430

    rails new ci-5644-abr-jul-2013
    Sólo contiene la creación del repositorio, no se han hecho
    modificaciones a la salida de rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ci5644AbrJul2013::Application.config.secret_token = '6d41b08f4e2aa69706ba7f692218c87b72b734f6f5387330e1d8089605850922bc7b512f9a12523f72f55d281ddf41172a31aa7718b2838d2d6e468ff303b1f5'
