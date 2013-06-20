23bfb0b62a02852f2cbeb53a9f82e27b50912dff
Andras Gyomrey <lgandras@gmail.com>
2013-06-20 16:53:09 -0430

    permisos

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,9 @@ class User < ActiveRecord::Base
   has_many :answers
   has_many :comments
   has_many :votes
+
+  before_save :default_values
+  def default_values
+    self.rol ||= 'regular'
+  end
 end
